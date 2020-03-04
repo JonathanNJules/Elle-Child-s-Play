5e438de066efce239048581fa1b9d85f0ea8dec6
Jonathan <jonathannjules@gmail.com>
2020-03-04 00:31:17 -0500

    First Block Manipulation Demo. Set up project for Windows MR.
    Imported package for and set up Windows MR support (so I can test with my second headset)
    Wrote script that can grab grip button input from controller, raycast out from hand position, and grab and manipulate blocks
    Set up little translucent beam to make it easier for users to gauge where their hands are pointing
    Slight update to lighting

---
+++ Assets/Scripts/BlockGameHand.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class BlockGameHand : MonoBehaviour
+{
+    public float gripValue;
+    public Transform currentBlock;
+    private RaycastHit hit;
+    public GameObject beam;
+
+    InputDevice rightHand;
+    public float gripThreshold = 0.6f;
+    public bool rightGripDown, rightGripReset = true;
+
+    void Start()
+    {
+        var rightHandDevices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, rightHandDevices);
+        rightHand = rightHandDevices[0];
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rightHand.TryGetFeatureValue(CommonUsages.grip, out gripValue);
+
+        if (rightGripDown == true)
+            rightGripDown = false;
+        if (gripValue > gripThreshold && rightGripReset == true)
+        {
+            rightGripDown = true;
+            rightGripReset = false;
+        }
+        if (rightGripReset == false && gripValue < gripThreshold)
+            rightGripReset = true;
+
+        if (currentBlock != null && gripValue == 0)
+            DropCurrentBlock();
+
+        if (rightGripDown == true)
+            print("iss tru");
+
+        if (currentBlock != null)
+            currentBlock.GetComponent<Rigidbody>().velocity = new Vector3(100, 200, 300);
+
+
+        bool seeBlock = false;
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.green);
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.transform.CompareTag("Block"))
+            {
+                seeBlock = true;
+                if(rightGripDown == true && currentBlock != hit.transform)
+                    ChangeCurrentBlock(hit.transform);
+            }
+        }
+
+        if (seeBlock && beam.activeSelf == false)
+            beam.SetActive(true);
+        else if (!seeBlock && beam.activeSelf == true)
+            beam.SetActive(false);
+    }
+
+    void ChangeCurrentBlock(Transform newBlock)
+    {
+        if (currentBlock != null)
+        {
+            currentBlock.GetComponent<Rigidbody>().isKinematic = false;
+            currentBlock.parent = null;
+        }
+
+        currentBlock = newBlock;
+        currentBlock.GetComponent<Rigidbody>().isKinematic = true;
+        currentBlock.parent = transform;
+    }
+
+    void DropCurrentBlock()
+    {
+        if (currentBlock != null)
+        {
+            currentBlock.GetComponent<Rigidbody>().isKinematic = false;
+            currentBlock.parent = null;
+            currentBlock = null;
+        }
+    }
+}
